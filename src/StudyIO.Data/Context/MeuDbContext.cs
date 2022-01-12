5b0ee778c87036dc67884c171bbb763f5e21893f
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-12 13:57:09 -0300

    Instalando o EF Core e configurando o DbContext

---
+++ src/StudyIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyIO.Business.Models;
+
+namespace StudyIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
