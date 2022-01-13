e63ad91021827a0eda4d56fec869e46daff8ad95
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-13 18:42:41 -0300

    Adicionando FornecedorId Em ProdutoViewModel

--- src/StudyIO.App/ViewModels/ProdutoViewModel.cs
+++ src/StudyIO.App/ViewModels/ProdutoViewModel.cs
@@ -14,6 +14,10 @@ namespace StudyIO.App.ViewModels
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Fornecedor")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
 
