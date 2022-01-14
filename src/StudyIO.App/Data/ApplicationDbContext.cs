a8eb7354e53fa8426b122065711669b7f436d4cc
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-14 17:06:22 -0300

    Customizando views

--- src/StudyIO.App/Data/ApplicationDbContext.cs
+++ src/StudyIO.App/Data/ApplicationDbContext.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using StudyIO.App.ViewModels;
 
 namespace StudyIO.App.Data
 {
