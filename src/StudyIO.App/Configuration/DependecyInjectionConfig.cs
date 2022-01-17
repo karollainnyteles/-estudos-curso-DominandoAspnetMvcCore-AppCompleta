42ddb3666017aeed86c2e84ed5498696642a11dd
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-17 16:40:29 -0300

    Organizando e otimizando a estrutura da app

---
+++ src/StudyIO.App/Configuration/DependecyInjectionConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+using StudyIO.App.Extensions;
+using StudyIO.Business.Interfaces;
+using StudyIO.Data.Context;
+using StudyIO.Data.Repositories;
+
+namespace StudyIO.App.Configuration
+{
+    public static class DependecyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependecies(this IServiceCollection services)
+        {
+            services.AddScoped<MeuDbContext>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
+
+            return services;
+        }
+    }
+}
