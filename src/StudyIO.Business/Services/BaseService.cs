c87b9eb20acb6b0edb70862a07e5c1d2b1b2ec59
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-17 20:26:49 -0300

    Validando entidade de negócios

---
+++ src/StudyIO.Business/Services/BaseService.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using StudyIO.Business.Models;
+
+namespace StudyIO.Business.Services
+{
+    public abstract class BaseService
+    {
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Notificar(error.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            // _notificador.Handle(new Notificacao(mensagem));
+        }
+
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return false;
+        }
+    }
+}
---
+++ src/StudyIO.Business/Services/ProdutoService.cs
@@ -0,0 +1,26 @@
+﻿using StudyIO.Business.Interfaces;
+using StudyIO.Business.Models;
+using StudyIO.Business.Models.Validations;
+using System;
+using System.Threading.Tasks;
+
+namespace StudyIO.Business.Services
+{
+    public class ProdutoService : BaseService, IProdutoService
+    {
+        public async Task Adicionar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Atualizar(Produto produto)
+        {
+            if (!ExecutarValidacao(new ProdutoValidation(), produto)) return;
+        }
+
+        public async Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
